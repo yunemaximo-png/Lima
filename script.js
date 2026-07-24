e031290e71d5893e294d7ebfc1e3ce7da410141a
yunemaximo-png <yune270909@gmail.com>
2026-07-24 13:50:39 -0300

    Comandos simples

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const paragrafo = document.querySelector("#paragrafo");
         const botao = document.querySelector("#botao");
         botao.addEventListener("click", () => {
-            paragrafo.innerHTML = "Mas as moças são lerdas."; 
+            paragrafo.innerHTML = "Mas não consegue reverter"; 
         });
          
